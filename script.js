636b70348e3149d27aa965f2fb0cdfdc163434a7
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-09 17:25:14 +0100

    final touches

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let form = document.querySelector('form')
 let username = document.querySelector('#user')
 let terms = document.querySelector('#terms')
 let selected = document.querySelector('#clubs')
-// let selectClub = document.querySelector("#clubs");
-// form.addEventListener('submit',function(e){
-//     e.preventDefault()
-//     console.log('username :' ,username.value)
-//     console.log('TermsResponse :' ,terms.checked)
-//     console.log('selected Club :' ,selectClub.value)
-// })
-// using input events and change
+let selectClub = document.querySelector("#clubs");
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    console.log('username :' ,username.value)
+    console.log('TermsResponse :' ,terms.checked)
+    console.log('selected Club :' ,selectClub.value)
+})
+//using input events and change
 let information= {}
 for (let input of [username,terms,selected]) {
-    input.addEventListener('input',({target})=>{
+    input.addEventListener('change',({target})=>{
        const{name,value,checked,type} = target
        information[name] = type == 'checkbox' ? checked : value
        console.log(information)
